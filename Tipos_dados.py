76f4ec99b8350f58feb75f156cde68596abd51e9
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2025-12-04 16:50:43 -0300

    Add files via upload

---
+++ Tipos_dados.py
@@ -0,0 +1,14 @@
+text =  'text'
+inteiro  =  10
+real = 5.8
+boleano  = True
+
+
+john = 3
+maria = 5
+adam =  6
+
+print(john, maria, adam)
+total_apples  =  john + maria +adam
+
+print(total_apples)
